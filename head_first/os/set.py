6195e56e1aa6aaaa0f98913e114910f1ebcf8d2d
Fankx <fankx@startimes.com.cn>
2018-02-11 11:33:00 +0800

    set

---
+++ head_first/os/set.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+dis = {1, 2, 5, 3, 21, 2, 21, 1, 2, 1}
+print(dis)
+minutes = [5, 6, 7, 8, 2, 1, 1, 2, 4]
+minutes_set = set(minutes)
+print(minutes_set)
