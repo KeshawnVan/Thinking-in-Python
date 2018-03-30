2c65f3a6c90990ccf27651226fc9ce23bfe4edaf
Fankx <fankx@startimes.com.cn>
2018-03-30 11:26:51 +0800

    sorted

--- head_first/os/sorted.py
+++ head_first/os/sorted.py
@@ -1,6 +1,31 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-data = [5, 6, 7, 8, 2, 1, 1, 2, 4]
+data = [5, 6, -7, 8, 2, -1, 1, 2, 4]
 new_data = sorted(data)
 print(data)
 print(new_data)
+
+print(sorted(data, key=abs))
+
+words = ['bob', 'about', 'Zoo', 'Credit']
+print(sorted(words))
+print(sorted(words, key=str.lower))
+print(sorted(words, key=str.lower, reverse=True))
+
+L = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
+
+
+def by_name(t):
+    return t[0]
+
+
+L2 = sorted(L, key=by_name)
+print(L2)
+
+
+def by_score(t):
+    return t[1]
+
+
+L3 = sorted(L, key=by_score, reverse=True)
+print(L3)
